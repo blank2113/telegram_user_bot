591e2ac06e7db53f8ae14da6220a31da4d220024
blank2113 <blank2113@github.com>
2025-11-20 20:54:14 +0500

    change wheel visiality page

--- src/pages/WheelPage.tsx
+++ src/pages/WheelPage.tsx
@@ -2,7 +2,7 @@ import FourtuneWheel from "./FortuneWheel";
 
 const WheelPage = () => {
   return (
-    <div className='w-full h-full px-3 overflow-y-scroll pt-10 pb-35 flex-1 flex justify-center items-center'>
+    <div className='w-full h-full px-3 overflow-y-scroll pt-10 pb-35 '>
       <FourtuneWheel />
     </div>
   );
